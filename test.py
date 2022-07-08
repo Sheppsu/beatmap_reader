913894f8af9e080083d2a9838c5203fe9235e912
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2022-07-08 02:10:46 -0400

    test with 5 objects and from random maps

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from beatmap_reader import SongsFolder, HitObjectType, CurveType
 import pygame
+import random
 
 
 songs = "C:\\Users\\Sheep\\Desktop\\osu!\\Songs"
@@ -12,43 +13,35 @@ if not folder.beatmapsets:
 
 
 def get_sliders():
-    sliders = []
-    slider_check = {
-        CurveType.PERFECT: False,
-        CurveType.BEZIER: False,
-        CurveType.CATMULL: False,
-        CurveType.LINEAR: False,
+    sliders = {
+        CurveType.PERFECT: [],
+        CurveType.BEZIER: [],
+        CurveType.CATMULL: [],
+        CurveType.LINEAR: [],
     }
 
-    for beatmapset in folder:
+    beatmapsets = list(folder.beatmapsets)
+    random.shuffle(beatmapsets)
+    for beatmapset in beatmapsets:
         if not beatmapset.beatmaps:
             print(f"Beatmapset {beatmapset.path} has no beatmaps. This is an error with the program.")
             break
         for beatmap in beatmapset:
             beatmap.load()
             for obj in beatmap.hit_objects:
-                if obj.type == HitObjectType.SLIDER and not slider_check[obj.curve.type]:
-                    sliders.append(obj)
-                    slider_check[obj.curve.type] = True
+                if obj.type == HitObjectType.SLIDER and len(sliders[obj.curve.type]) < 5:
+                    sliders[obj.curve.type].append(obj)
                     print(f"Using {obj.curve.type} slider from {beatmap.path}")
-                if all(slider_check.values()):
-                    return sliders
+                    if all(map(lambda l: len(l) == 5, sliders.values())):
+                        return sliders
 
     raise Exception("Could not find a slider of each type.")
 
 
-def is_implemented(slider):
-    try:
-        slider.curve.create_curve_functions()
-    except NotImplementedError:
-        return False
-    return True
-
-
 sliders = get_sliders()
-sliders = [slider for slider in sliders if is_implemented(slider)]
-for slider in sliders:
-    slider.render()
+for slider_list in sliders.values():
+    for slider in slider_list:
+        slider.render((640, 480), (0, 0))
 
 
 pygame.init()
@@ -56,11 +49,11 @@ screen = pygame.display.set_mode((640, 480))
 clock = pygame.time.Clock()
 
 draw_points = True
-draw_sliders = {
-    CurveType.PERFECT: True,
-    CurveType.BEZIER: True,
-    CurveType.CATMULL: True,
-    CurveType.LINEAR: True,
+slider_indexes = {
+    CurveType.PERFECT: 0,
+    CurveType.BEZIER: 0,
+    CurveType.CATMULL: 0,
+    CurveType.LINEAR: 0,
 }
 while True:
     for event in pygame.event.get():
@@ -72,18 +65,29 @@ while True:
             if event.key == pygame.K_p:
                 draw_points = not draw_points
             if event.key in slider_keys:
-                slider_type = list(draw_sliders.keys())[slider_keys.index(event.key)]
-                draw_sliders[slider_type] = not draw_sliders[slider_type]
+                slider_type = list(slider_indexes.keys())[slider_keys.index(event.key)]
+                slider_indexes[slider_type] = -1 if slider_indexes[slider_type] != -1 else 0
+            if event.key in [pygame.K_LEFT, pygame.K_RIGHT]:
+                slider_type = [type for type, i in slider_indexes.items() if i != -1]
+                if not slider_type:
+                    continue
+                slider_type = slider_type[0]
+                slider_indexes[slider_type] += 1 if event.key == pygame.K_RIGHT else -1
+                if slider_indexes[slider_type] > 4:
+                    slider_indexes[slider_type] = 0
+                elif slider_indexes[slider_type] < 0:
+                    slider_indexes[slider_type] = 4
 
     screen.fill((0, 0, 0))
 
-    for slider in sliders:
-        if not draw_sliders[slider.curve.type]:
+    for slider_type, i in slider_indexes.items():
+        if i == -1:
             continue
+        slider = sliders[slider_type][i]
         screen.blit(slider.surf, (0, 0))
         if draw_points:
             for point in slider.curve.points:
-                pygame.draw.circle(screen, (255, 255, 255), (point[0]+64, point[1]+48), 2)
+                pygame.draw.circle(screen, (255, 0, 0), (point[0], point[1]), 2)
 
     pygame.display.update()
     clock.tick(60)

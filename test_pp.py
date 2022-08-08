b7244efea784294719a92fc82b034a60eeab3335
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2022-08-08 01:51:20 -0400

    tests

--- test_pp.py
+++ test_pp.py
@@ -1,25 +1,27 @@
-from osu import Mods
-from beatmap_reader import SongsFolder
+from osu import Client, GameModeStr
+from beatmap_reader import OsuPerformanceCalculator, OsuDifficultyAttributes, OsuScoreAttributes
+import os
+
+score_id = 4139164832
+client = Client.from_client_credentials(int(os.getenv('osu_client_id')), os.getenv('osu_client_secret'), os.getenv('osu_redirect_uri'))
+
+score = client.get_score_by_id(GameModeStr.STANDARD, score_id)
+beatmap = client.get_beatmap(score.beatmap.id)
+beatmap_attributes = client.get_beatmap_attributes(beatmap.id, score.mods, score.mode)
 
 
-songs = SongsFolder.from_path("C:\\Users\\Sheep\\Desktop\\osu!\\Songs")
-
-print(len(songs.beatmapsets))
-beatmapset = songs.beatmapsets[2029]
-beatmap = beatmapset.beatmaps[0]
-print(beatmap.path)
-if not beatmap.load():
-    quit()
-
-
-nomod = beatmap.get_difficulty_attributes()
-print(f"No mod: {nomod.star_rating} {nomod.aim_strain} {nomod.speed_strain} "
-      f"{nomod.flashlight_rating} {nomod.slider_factor}")
-
-difficulty = []
-for mod in (Mods.HardRock, Mods.DoubleTime, Mods.Easy, Mods.HalfTime, Mods.Flashlight):
-    difficulty.append(beatmap.get_difficulty_attributes(mod))
-
-for diff in difficulty:
-    print(diff.mods, diff.star_rating, diff.aim_strain, diff.speed_strain,
-          diff.flashlight_rating, diff.slider_factor)
+calculator = OsuPerformanceCalculator(
+    GameModeStr.STANDARD,
+    OsuDifficultyAttributes.from_attributes({
+        'aim_strain': beatmap_attributes.mode_attributes.aim_difficulty,
+        'speed_strain': beatmap_attributes.mode_attributes.speed_difficulty,
+        'flashlight_rating': beatmap_attributes.mode_attributes.flashlight_difficulty,
+        'slider_factor': beatmap_attributes.mode_attributes.slider_factor,
+        'approach_rate': beatmap_attributes.mode_attributes.approach_rate,
+        'overall_difficulty': beatmap_attributes.mode_attributes.overall_difficulty,
+        'drain_rate': beatmap.drain,
+        'hit_circle_count': beatmap.count_circles,
+        'slider_count': beatmap.count_sliders,
+        'spinner_count': beatmap.count_spinners,
+    }), OsuScoreAttributes.from_osupy_score(score))
+print(calculator.calculate())

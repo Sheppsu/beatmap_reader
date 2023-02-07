9bd2ffff4c998601495f8c35cd4bc482e251d6f4
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2023-02-07 00:39:05 -0600

    sliderpath module

--- setup.py
+++ setup.py
@@ -35,13 +35,18 @@ classifiers = [
 
 packages = [
     'beatmap_reader',
-    'beatmap_reader.sliderpath'
 ]
 
 setuptools.setup(
     name="beatmap_reader",
     version=version,
     packages=packages,
+    ext_modules=[
+        setuptools.Extension(
+            name="sliderpath",
+            sources=["beatmap_reader/sliderpathsrc/sliderpath.c"],
+        ),
+    ],
     author="Sheepposu",
     description="Library for easily reading beatmaps",
     long_description=readme,

ecb1eab165ee5307632e42fc48b07542c743c2a7
Sheppsu <49356627+Sheppsu@users.noreply.github.com>
2024-10-25 16:46:16 -0400

    update test

--- test_map.py
+++ test_map.py
@@ -1,8 +1,5 @@
 from beatmap_reader import Beatmap
 
 
-songs = "C:\\Users\\Sheep\\Desktop\\osu!\\Songs"
-
-
 beatmap = Beatmap.from_path("C:\\Users\\Sheep\\Desktop\\osu!\\Songs\\829629 antiPLUR - Runengon\\antiPLUR - Runengon (Yusomi) [ar10].osu")
 beatmap.load()

94a0e1d47ac47df016e8db03a737a2c03790c1f4
Sheppsu <49356627+Sheppsu@users.noreply.github.com>
2024-10-25 17:12:08 -0400

    reformat test

--- test_map.py
+++ test_map.py
@@ -1,7 +1,9 @@
 from beatmap_reader import Beatmap
 
 
-# beatmap = Beatmap.from_path("C:\\Users\\Sheeppsu\\Desktop\\osu!\\Songs\\829629 antiPLUR - Runengon\\antiPLUR - Runengon (Yusomi) [ar10].osu")
-beatmap = Beatmap.from_path("C:\\Users\\Sheeppsu\\Desktop\\osu!\\Songs\\3 Ni-Ni - 1,2,3,4, 007 [Wipeout Series]\\Ni-Ni - 1,2,3,4, 007 [Wipeout Series] (MCXD) [-Breezin-].osu")
+path_to_osu_file = "..."
+
+
+beatmap = Beatmap.from_path(path_to_osu_file)
 beatmap.load()  # load data from file
 beatmap.load_objects()  # calculate slider paths, stacking, and max combo
